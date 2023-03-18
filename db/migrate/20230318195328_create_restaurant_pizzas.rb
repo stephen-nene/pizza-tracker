eb195cfc5a666f3bf6a593147e3f8f89ff039290
stephen-nene <stephen.nene@student.moringaschool.com>
2023-03-18 22:57:07 +0300

    run migration

--- db/migrate/20230318195328_create_restaurant_pizzas.rb
+++ db/migrate/20230318195328_create_restaurant_pizzas.rb
@@ -1,7 +1,7 @@
 class CreateRestaurantPizzas < ActiveRecord::Migration[6.1]
   def change
     create_table :restaurant_pizzas do |t|
-      t.decimal :price
+      t.decimal :price, precision: 5, scale: 2
       t.references :restaurant, null: false, foreign_key: true
       t.references :pizza, null: false, foreign_key: true
 
