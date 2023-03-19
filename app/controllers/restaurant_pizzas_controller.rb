27772a760cdc4a8bc428fa9396160c1a6470454b
stephen-nene <stephen.nene@student.moringaschool.com>
2023-03-19 14:48:26 +0300

    creating a post for a new pizza creation

--- app/controllers/restaurant_pizzas_controller.rb
+++ app/controllers/restaurant_pizzas_controller.rb
@@ -1,6 +1,6 @@
 
 class RestaurantPizzasController < ApplicationController
-  # GETs from /restaurants/3/pizzas
+  # GET /restaurants/3/pizzas
   def index
     @restaurant = Restaurant.find(params[:restaurant_id])
     @pizzas = @restaurant.pizzas
@@ -9,5 +9,44 @@ class RestaurantPizzasController < ApplicationController
       pizzas: @pizzas.as_json(only: [:id, :name, :ingredients]),
       prices_id: @restaurant.restaurant_pizzas.as_json(only: [:id, :price, :pizza_id, :restaurant_id])
     }
+  end
+
+  # GET /restaurants/:id/pizzas/:id
+  def show
+    @restaurant = Restaurant.find_by(id: params[:restaurant_id])
+    if @restaurant
+      @pizza = Pizza.find_by(id: params[:id])
+      if @pizza
+        @restaurant_pizza = RestaurantPizza.find_by(restaurant: @restaurant, pizza: @pizza)
+        if @restaurant_pizza
+          render json: {
+            pizza: @pizza.as_json(only: [:id, :name, :ingredients]),
+            price: @restaurant_pizza.as_json(except: [:created_at, :updated_at])
+          }
+        else
+          render json: { error: 'Restaurant pizza not found' }, status: :not_found
+        end
+      else
+        render json: { error: 'Pizza not found' }, status: :not_found
+      end
+    else
+      render json: { error: 'Restaurant not found' }, status: :not_found
+    end
+  end
+
+  # POST /restaurant_pizzas
+  def create
+    @restaurant_pizza = RestaurantPizza.new(pizza_params)
+    if @restaurant_pizza.save
+      render json: @restaurant_pizza.pizza.as_json(only: [:id, :name, :ingredients])
+    else
+      render json: { errors: @restaurant_pizza.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def pizza_params
+    params.require(:restaurant_pizza).permit(:price, :pizza_id, :restaurant_id, :ingredients)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :restaurants, only: [:index, :show, :destroy] do
-    resources :pizzas, only: [:index], controller: 'restaurant_pizzas'
+    resources :pizzas, only: [:index, :show], controller: 'restaurant_pizzas'
   end
   resources :pizzas, only: [:index]
   resources :restaurant_pizzas, only: [:create, :destroy]
